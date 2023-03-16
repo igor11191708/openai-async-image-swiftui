c48aa874f1a9f764f29da2cf0c2cd8ddfc2991f3
Igor <igorshelopaev@gmail.com>
2023-03-16 18:07:23 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/The-Igor/async-http-client.git", from: "1.4.3")
+        .package(url: "https://github.com/The-Igor/async-http-client.git", from: "1.4.4")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
