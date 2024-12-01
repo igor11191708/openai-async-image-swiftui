5cec6b959315a1cbe760927c5ac03e279e19bed9
Igor <igorshelopaev@gmail.com>
2024-12-01 11:43:46 +0100

    update

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/swiftuiux/async-http-client.git", from: "1.5.0"),
-        .package(url: "https://github.com/swiftuiux/async-task.git", from: "1.2.3")
+        .package(url: "https://github.com/swiftuiux/async-task.git", from: "1.2.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
